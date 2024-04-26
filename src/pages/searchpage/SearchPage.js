b1202928b9c50a33a5588966516d1b0355e86165
seongsu-kwon <mnbv1880@gmail.com>
2024-04-26 11:51:42 +0900

    SearchBar

--- src/pages/searchpage/SearchPage.js
+++ src/pages/searchpage/SearchPage.js
@@ -1,9 +1,26 @@
-import React from "react";
+import { InputBase, TextField, Typography } from '@mui/material';
+import SearchBar from 'components/SearchBar';
+import React from 'react';
 
 export default function SearchPage() {
-  return (
-    <div>
-      <h1>검색 페이지</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>검색 페이지</h1>
+            <SearchBar />
+            <Typography variant="h6">input</Typography>
+            <input
+                type="text"
+                placeholder="검색어를 입력하세요"
+                enterKeyHint="search"
+            />
+            <Typography variant="h6" enterKeyHint="search">
+                TextField
+            </Typography>
+            <TextField label="검색어를 입력하세요" />
+            <Typography variant="h6" enterKeyHint="search">
+                InputBase
+            </Typography>
+            <InputBase placeholder="검색어를 입력하세요" />
+        </div>
+    );
 }
